c4173a8d5866c2cf27098b855d3e78ba5eb211d9
Christian-Kr <cerebrosus@gmx.net>
2013-04-02 19:41:31 +0200

    Add functionality to CustomWebView.

---
+++ src/CustomWebView.cpp
@@ -0,0 +1,176 @@
+/* Copyright (c) 2012 Christian Krippendorf <Coding@Christian-Krippendorf.de>
+ *
+ * This file is part of qlamz.
+ *
+ * qlamz is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * qlamz is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * qlamz.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include "CustomWebView.h"
+
+#include "CustomWebPage.h"
+#include "PersistentCookieJar.h"
+
+#include <QUrl>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QWebFrame>
+#include <QWebPage>
+#include <QByteArray>
+#include <QDebug>
+
+
+CustomWebView::CustomWebView(QWidget *pParent)
+    : QWebView(pParent),
+    m_bDownloadMode(false),
+    m_pNetReply(NULL),
+    m_pWebPage(new CustomWebPage()),
+    m_pCookieJar(new PersistentCookieJar())
+{
+    // Set CustomWebPage object.
+    setPage(m_pWebPage);
+
+    m_pWebPage->setLinkDelegationPolicy(QWebPage::DontDelegateLinks);
+    m_pWebPage->setForwardUnsupportedContent(true);
+
+    m_pCookieJar->load();
+    m_pWebPage->networkAccessManager()->setCookieJar(m_pCookieJar);
+
+    // Build some connections.
+    connect(m_pWebPage, SIGNAL(formSubmitted(const QNetworkRequest&)),
+        this, SLOT(formSubmitted(const QNetworkRequest&)));
+
+    connect(m_pWebPage, SIGNAL(unsupportedContent(QNetworkReply*)), this,
+        SLOT(unsupportedContent(QNetworkReply*)));
+
+    connect(this, SIGNAL(loadFinished(bool)), this, SLOT(loadFinished(bool)));
+}
+
+CustomWebView::~CustomWebView()
+{
+    delete m_pWebPage;
+    delete m_pCookieJar;
+}
+
+QByteArray CustomWebView::cookieData() const
+{
+    return m_pCookieJar->data();
+}
+
+void CustomWebView::load(const QUrl &url)
+{
+    createNetworkReplyFromUrl(url);
+}
+
+QNetworkReply* CustomWebView::createNetworkReplyFromUrl(const QUrl &url)
+{
+    QNetworkRequest netRequest;
+    netRequest.setUrl(QUrl(url));
+    netRequest.setRawHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; "
+        "Windows NT 6.1)");
+
+    // Delete the last reply.
+    if (m_pNetReply != NULL) {
+        m_pNetReply->deleteLater();
+    }
+
+    m_pNetReply = m_pWebPage->networkAccessManager()->get(netRequest);
+
+    buildNetReplyConnections(m_pNetReply);
+
+    return m_pNetReply;
+}
+
+void CustomWebView::buildNetReplyConnections(const QNetworkReply *netReply)
+{
+    connect(netReply, SIGNAL(error(QNetworkReply::NetworkError)), this,
+        SLOT(netReplyError(QNetworkReply::NetworkError)));
+
+    connect(netReply, SIGNAL(finished()), this, SLOT(netReplyFinished()));
+}
+
+void CustomWebView::loadFinished(bool bOk)
+{
+    qDebug() << "Load finished";
+    m_pCookieJar->save();
+}
+
+void CustomWebView::netReplyError(QNetworkReply::NetworkError netError)
+{
+    qDebug() << "Network Error Occured: " << netError;
+    qDebug() << url();
+}
+
+void CustomWebView::netReplyFinished()
+{
+    qDebug() << "Finished";
+
+    if (m_pNetReply->rawHeader("Location").size() > 0) {
+        createNetworkReplyFromUrl(QUrl::fromEncoded(
+            m_pNetReply->rawHeader("Location"), QUrl::StrictMode));
+    } else {
+        // If we are in download mode we are ready.
+        if (m_bDownloadMode) {
+            m_bDownloadMode = false;
+
+            emit amzDownloaded(QString(m_pNetReply->readAll()));
+        } else {
+            QString strTest("<script type='text/javascript'>"
+                "var fake_navigator={};"
+                "for(var i in navigator)"
+                "{fake_navigator[i]=navigator[i];}"
+                "fake_navigator.platform='Win32';"
+                "fake_navigator.appCodeName='Mozilla';"
+                "fake_navigator.appName='Microsoft Internet Explorer';"
+                "fake_navigator.appVersion='4.0 (compatible; MSIE 8.0; Windows NT 6.1)';"
+                "fake_navigator.userAgent='Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)';"
+                "navigator=fake_navigator;"
+                "</script>");
+
+            QString content = strTest + QString::fromUtf8(m_pNetReply->readAll()).toAscii();
+
+            //m_pWebPage->noEmitFormSubmittedOnce();
+            setHtml(content, m_pNetReply->url());
+        }
+    }
+}
+
+void CustomWebView::formSubmitted(const QNetworkRequest &netRequest)
+{
+    qDebug() << "Submitted";
+
+    if (m_pNetReply != NULL) {
+        m_pNetReply->deleteLater();
+    }
+
+    m_pWebPage->noEmitFormSubmittedOnce();
+    m_pNetReply = m_pWebPage->networkAccessManager()->get(netRequest);
+
+    buildNetReplyConnections(m_pNetReply);
+}
+
+void CustomWebView::unsupportedContent(QNetworkReply *netReply)
+{
+    qDebug() << "Download Reply";
+
+    m_bDownloadMode = true;
+
+    if (m_pNetReply != NULL) {
+        m_pNetReply->deleteLater();
+    }
+
+    m_pNetReply = netReply;
+
+    buildNetReplyConnections(m_pNetReply);
+}
+
+
+
---
+++ src/CustomWebView.h
@@ -0,0 +1,111 @@
+/* Copyright (c) 2012 Christian Krippendorf <Coding@Christian-Krippendorf.de>
+ *
+ * This file is part of qlamz.
+ *
+ * qlamz is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * qlamz is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * qlamz.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef CUSTOMWEBVIEW_H
+#define CUSTOMWEBVIEW_H
+
+#include <QWebView>
+#include <QNetworkReply>
+
+
+class QByteArray;
+class CustomWebPage;
+class PersistentCookieJar;
+class QUrl;
+class QNetworkAccessManager;
+
+class CustomWebView : public QWebView
+{
+    Q_OBJECT
+
+public:
+    /**
+     * Constructor */
+    CustomWebView(QWidget *pParent = 0);
+
+    /**
+     * Destructor */
+    ~CustomWebView();
+
+    /**
+     * Return the actual cookie data.
+     *
+     * @return a QByteArray object with the data. */
+    QByteArray cookieData() const;
+
+public slots:
+    /**
+     * Called when a download was requested.
+     *
+     * @param netRequest The netRequest. */
+    void unsupportedContent(QNetworkReply *netReply);
+
+    /**
+     * Load an url.
+     *
+     * @param url The url that should be load. */
+    void load(const QUrl &url);
+
+    /**
+     * Will be called, when loading finished.
+     *
+     * @param bOk Everthing was ok while loading. */
+    void loadFinished(bool bOk);
+
+    /**
+     * Will be called, when getting teh network reply has finished. */
+    void netReplyFinished();
+
+    /**
+     * Will be called, when a network error occured.
+     *
+     * @param netError The network error code. */
+    void netReplyError(QNetworkReply::NetworkError netError);
+
+    /**
+     * Will be called, when a a form was submitted.
+     *
+     * @param netRequest The network request. */
+    void formSubmitted(const QNetworkRequest &netRequest);
+
+signals:
+    /**
+     * Emits when an amz file will be downloaded. */
+    void amzDownloaded(const QString &content);
+
+private:
+    bool m_bDownloadMode;
+
+    QNetworkReply *m_pNetReply;
+    CustomWebPage *m_pWebPage;
+    PersistentCookieJar *m_pCookieJar;
+
+    /**
+     * Build connections to the given netReply.
+     *
+     * @param netReply Reply to build connections with. */
+    void buildNetReplyConnections(const QNetworkReply *netReply);
+
+    /**
+     * Create a netReply from the url.
+     *
+     * @param url The url to create a network reply from.
+     * @return The new netReply object. */
+    QNetworkReply* createNetworkReplyFromUrl(const QUrl &url);
+};
+
+
+#endif // CUSTOMWEBVIEW_H
